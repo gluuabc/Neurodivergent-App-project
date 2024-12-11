4c475c38f9bdd83dd5d04dc1826a03f507ebbf28
gluuabc <stella0518lyx@gmail.com>
2024-12-11 13:41:03 -0800

    Update bar.dart

--- flutter_application_1/lib/bar.dart
+++ flutter_application_1/lib/bar.dart
@@ -17,31 +17,44 @@ class CustomBottomBar extends StatelessWidget {
       case 0:
         Navigator.pushReplacement(
           context,
-          MaterialPageRoute(builder: (context) => const FirstRoute(appTitle: 'Neurodivergent App', name:'Timer Track')),
+          MaterialPageRoute(
+            builder: (context) => const FirstRoute(
+              appTitle: 'Neurodivergent App',
+              name: 'Timer Track',
+            ),
+          ),
         );
         break;
       case 1:
         Navigator.pushReplacement(
           context,
-          MaterialPageRoute(builder: (context) => const SecondRoute(appTitle: 'Neurodivergent App')),
+          MaterialPageRoute(
+            builder: (context) => const SecondRoute(appTitle: 'Neurodivergent App'),
+          ),
         );
         break;
       case 2:
         Navigator.pushReplacement(
           context,
-          MaterialPageRoute(builder: (context) => const ThirdRoute(appTitle: 'Neurodivergent App')),
+          MaterialPageRoute(
+            builder: (context) => const ThirdRoute(appTitle: 'Neurodivergent App'),
+          ),
         );
         break;
       case 3:
         Navigator.pushReplacement(
           context,
-          MaterialPageRoute(builder: (context) => const FourthRoute(appTitle: 'Neurodivergent App')),
+          MaterialPageRoute(
+            builder: (context) => const FourthRoute(appTitle: 'Neurodivergent App'),
+          ),
         );
         break;
       case 4:
         Navigator.pushReplacement(
           context,
-          MaterialPageRoute(builder: (context) => const FifthRoute(appTitle: 'Neurodivergent App')),
+          MaterialPageRoute(
+            builder: (context) => const FifthRoute(appTitle: 'Neurodivergent App'),
+          ),
         );
         break;
     }
@@ -53,25 +66,40 @@ class CustomBottomBar extends StatelessWidget {
       type: BottomNavigationBarType.fixed,
       currentIndex: currentIndex,
       onTap: (index) => _onItemTapped(context, index),
-      items: const [
+      items: [
         BottomNavigationBarItem(
-          icon: Icon(Icons.alarm),
+          icon: Icon(
+            Icons.alarm,
+            color: currentIndex == 0 ? const Color.fromARGB(255, 243, 166, 33) : Colors.white,
+          ),
           label: 'Alarm',
         ),
         BottomNavigationBarItem(
-          icon: Icon(Icons.add_circle_outlined),
+          icon: Icon(
+            Icons.add_circle_outlined,
+            color: currentIndex == 1 ? const Color.fromARGB(255, 243, 166, 33): Colors.white,
+          ),
           label: 'Add',
         ),
         BottomNavigationBarItem(
-          icon: Icon(Icons.home),
+          icon: Icon(
+            Icons.home,
+            color: currentIndex == 2 ? const Color.fromARGB(255, 243, 166, 33) : Colors.white,
+          ),
           label: 'Home',
         ),
         BottomNavigationBarItem(
-          icon: Icon(Icons.calendar_month_outlined),
+          icon: Icon(
+            Icons.calendar_month_outlined,
+            color: currentIndex == 3 ? const Color.fromARGB(255, 243, 166, 33) : Colors.white,
+          ),
           label: 'Calendar',
         ),
         BottomNavigationBarItem(
-          icon: Icon(Icons.settings_outlined),
+          icon: Icon(
+            Icons.settings_outlined,
+            color: currentIndex == 4 ? const Color.fromARGB(255, 243, 166, 33) : Colors.white,
+          ),
           label: 'Settings',
         ),
       ],
